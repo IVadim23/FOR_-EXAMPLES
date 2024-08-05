e2d2d810b0ce62586fbed40b627bc2f0ae4dbc96
IVadim23 <inazrski@gmail.com>
2024-08-05 21:04:33 +0300

    Add files via upload

---
+++ 1 to 10 for exsamples/08. Number sequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+
+            for (int number = 1; number <= n; number++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                if (value > maxNumber)
+                {
+                    maxNumber = value;
+                }
+                if (value < minNumber)
+                {
+                    minNumber = value;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
