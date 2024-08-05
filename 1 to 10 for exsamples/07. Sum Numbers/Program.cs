72a3c6c3ec0b3079209639aa4ee4f9faba0a91fc
IVadim23 <inazrski@gmail.com>
2024-08-05 21:03:42 +0300

    Add files via upload

---
+++ 1 to 10 for exsamples/07. Sum Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace _07._Sum_Numbers
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int n = int.Parse(Console.ReadLine()); //number of numbers 
+
+
+            //numbers from first to last
+            //repeat: read the value + sum the value
+            //start: the first number (1)
+            //end: ​​last number (n)
+            //change: +1
+
+            int sum = 0; //sum of numbers
+
+            for (int number = 1; number <= n; number++)
+                {
+                    int value = int.Parse(Console.ReadLine()); //the value of the number
+                sum = sum + value; //sum += value;
+
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+
+  
