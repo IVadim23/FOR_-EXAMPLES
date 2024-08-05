5254d9fafd9bbf5543256951a1470cabf433f83b
IVadim23 <inazrski@gmail.com>
2024-08-05 20:58:25 +0300

    Add files via upload

---
+++ 1 to 10 for exsamples/03. Numbers 1.N with Step 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03._Numbers_1.N_with_Step_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int n =  int.Parse(Console.ReadLine());
+            for (int num = 1; num <= n; num += 3)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
