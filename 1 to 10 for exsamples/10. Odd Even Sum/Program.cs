96488644aac1a0a611d800d56158c49e5150ef8f
IVadim23 <inazrski@gmail.com>
2024-08-05 21:06:08 +0300

    Add files via upload

---
+++ 1 to 10 for exsamples/10. Odd Even Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P10
+{
+    class EvenOddSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int number = 1; number <= n; number++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    evenSum += value;
+                }
+                else
+                {
+                    oddSum += value;
+                }
+
+            }
+
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(evenSum - oddSum)}");
+            }
+        }
+    }
+}
