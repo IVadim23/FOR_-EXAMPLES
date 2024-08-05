a2a95304a5d222c39eefb8e918f92ada5c84cd52
IVadim23 <inazrski@gmail.com>
2024-08-05 21:00:31 +0300

    Add files via upload

---
+++ 1 to 10 for exsamples/05. Character Sequence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05._Character_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         string tex = Console.ReadLine();
+            for (int pos = 0; pos <= tex.Length-1; pos++)
+            {
+                char c = tex[pos];  
+                Console.WriteLine(c);
+            }
+
+        }
+    }
+}
