3dd084c604482dca07be5ae59f0aa74d1c4cf481
IVadim23 <inazrski@gmail.com>
2024-08-05 21:02:29 +0300

    Add files via upload

---
+++ 1 to 10 for exsamples/06. Vowels Sum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _06._Vowels_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();//all positions in the text(0 to last
+
+            //we repeat: take the position symbol + check
+            //start: 0
+            //end: length - 1
+            //change: +1
+
+            int sum = 0; //sum of the vowels
+            for (int position = 0; position <= text.Length - 1; position++)
+            {
+                char symbol = text[position];
+                //checking if it is: a, e, i, o ,u
+
+                if (symbol == 'a')
+                {
+                    sum += 1;
+                }
+                else if (symbol == 'e')
+                {
+                    sum += 2;
+                }
+                else if (symbol == 'i')
+                {
+                    sum += 3;
+                }
+                else if (symbol == 'o')
+                {
+                    sum += 4;
+                }
+                else if (symbol == 'u')
+                {
+                    sum += 5;
+                }
+            }
+
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
+
